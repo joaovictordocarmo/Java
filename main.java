6e163ae29904273bf345c67a8836af5793cbf705
joao victor <128156864+joaovictordocarmo@users.noreply.github.com>
2024-10-31 08:17:16 -0300

    .

---
+++ main.java
@@ -0,0 +1,13 @@
+class helloworld {
+     public static void main(String[] args){
+       System.out.println("Esse e o Screen match");
+       System.out.println("Filme: top gun: maverick");
+        
+       int ano = 2022;
+       boolean incluidoNoPlano = true;
+       double notaFilme = 8.1;
+        
+        System.out.println("ano do lancamento " + ano);
+        
+     }
+}
