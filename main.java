9675197288109daab5f3529ebb29aa9bf575a571
joao victor <128156864+joaovictordocarmo@users.noreply.github.com>
2024-11-06 08:56:21 -0300

    .

--- main.java
+++ main.java
@@ -8,6 +8,13 @@ class helloworld {
        double notaFilme = 8.1;
         
         System.out.println("ano do lancamento " + ano);
+
+
+        double media = (8.4 + 7.5 + 6.8)/3;
+        System.out.println("A media das notas deste filme e:" +media);
+        string sinopse;
+        sinopse = "Depois de mais de 30 anos de serviço como um dos principais aviadores da Marinha, Pete "Maverick" Mitchell está de volta, rompendo os limites como um piloto de testes corajoso. No mundo contemporâneo das guerras tecnológicas, Maverick enfrenta drones e prova que o fator humano ainda é essencial.";
+        System.out.println(sinopse);
         
      }
 }
